dc3f7f434a6e91c49731afaca0cb86413f44c975
CharlesBoydelaTour <charles.boydelatour@gmail.com>
2022-11-13 16:53:37 +0100

    adding the file .py loader to app

--- src/pipeline_process.py
+++ src/pipeline_process.py
@@ -12,13 +12,13 @@ def run_string_code(code:str):
     exec(code, glob)
 
 def run_file_code(file_path:str):
-    #function to run a file as code
+    #function to run a python file as code
     with open(file_path, 'r') as f:
         code = f.read()
-    glob = {}
-    exec(code, glob)
+    run_string_code(code)
+    return code
 
-def process(code_to_correct:str, country='FRA'):
+def process(code_to_correct, country='FRA'):
     #if it exists, supress emissions.csv
     try:
         os.remove("emissions.csv")
@@ -28,15 +28,21 @@ def process(code_to_correct:str, country='FRA'):
     #function to run the code and track the carbon emissions
     @carbon_tracker
     def main_notcorrectec():
+        global code
         #function to run the code to correct
-        run_string_code(code_to_correct)
+        if code_to_correct.endswith('.py'):
+            code = run_file_code(code_to_correct)
+        else: 
+            run_string_code(code_to_correct)
+            code = code_to_correct
         #read the emissions.csv file and print the last line
+        return code
     
     data_em = pd.read_csv("emissions.csv")
     #get emissions of the last line
     emissions_notcorrected = data_em.iloc[-1]["emissions"]
     
-    corrected_code = improve_code.correct_code(code_to_correct)
+    corrected_code = improve_code.correct_code(code)
     corrected_code_str = improve_code.select_code_from_output(corrected_code)
     print(f"{corrected_code_str}")
     
@@ -53,35 +59,7 @@ def process(code_to_correct:str, country='FRA'):
     return corrected_code_str, emissions_notcorrected, emissions_corrected, percent_reduction
     
 if __name__ == "__main__":   
-    code_to_correct = """
-# Program to display the Fibonacci sequence up to n-th term
-
-nterms = 5
-
-# first two terms
-n1, n2 = 0, 1
-count = 0
-
-# check if the number of terms is valid
-if nterms <= 0:
-   print('Please enter a positive integer')
-# if there is only one term, return n1
-elif nterms == 1:
-   print(' sequence upto',nterms,':')
-   print(n1)
-# generate fibonacci sequence
-else:
-   print('Fibonacci sequence:')
-   while count < nterms:
-       print(n1)
-       nth = n1 + n2
-       # update values
-       n1 = n2
-       n2 = nth
-       count += 1
- 
-    """
-    
+    code_to_correct = "test.py"
     corrected_code_str, emissions_notcorrected, emissions_corrected, percent_reduction = process(code_to_correct)
     #print corrected code
     print(f"{corrected_code_str}")
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import src.pipeline_process as pp
+from io import StringIO
 
 st.set_page_config(layout="wide")
 
@@ -14,8 +15,9 @@ col1, col2 = st.columns(2)
     
 ## Create 2 boxes in the page
 #1st box is input code, 2nd box is output code
-#input code 
-code = col1.text_area("Input Code", height=500)
+#input code either text or file
+code = col1.text_area("Input code", height=500)
+code2 = col1.file_uploader("Upload file", type=['py'])
 #code = col1.file_uploader("Enter your  code here", key="code")
 country_code = col1.text_input("Enter your country code", key="country_code")
 #output code
@@ -24,5 +26,13 @@ code_corrected = col2.code('''#Output Code''', language="python")
 #When the button is clicked, the code is processed and the output is displayed in the 2nd box
 if code:
     corrected_code_str, emissions_notcorrected, emissions_corrected, percent_reduction = pp.process(code_to_correct=code, country=country_code)
+    code_corrected.code(corrected_code_str, language="python")
+    col2.text("Improvement: {}%".format(percent_reduction))
+
+if code2:
+    stringio = StringIO(code2.getvalue().decode("utf-8"))
+    code_to_read = stringio.read()
+    col2.text("Improvement: {}%".format(code_to_read))
+    corrected_code_str, emissions_notcorrected, emissions_corrected, percent_reduction = pp.process(code_to_correct=code_to_read, country=country_code)
     code_corrected.code(corrected_code_str, language="python")
     col2.text("Improvement: {}%".format(percent_reduction))
