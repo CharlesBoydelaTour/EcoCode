8cf1721df59da6fca4fb26e25ef4b2d05349f8e9
CharlesBoydelaTour <charles.boydelatour@gmail.com>
2022-11-13 17:12:32 +0100

    better styling streamlit_app

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,14 +12,23 @@ print(os.getcwd())
 #    st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
 
 col1, col2 = st.columns(2)
-    
+#put title at column 1 (Input) and column 2 (Output)
+with col1:
+    st.title('Input')
+with col2:
+    st.title('Recommendation')
+#set a separator element between the two columns
+st.markdown('---')
+
 ## Create 2 boxes in the page
 #1st box is input code, 2nd box is output code
 #input code either text or file
+col1.subheader('Add country code and Input Code (Text or File)')
+country_code = col1.text_input("Enter your country code", key="country_code")
+
 code = col1.text_area("Input code", height=500)
 code2 = col1.file_uploader("Upload file", type=['py'])
-#code = col1.file_uploader("Enter your  code here", key="code")
-country_code = col1.text_input("Enter your country code", key="country_code")
+
 #output code
 code_corrected = col2.code('''#Output Code''', language="python")
 
@@ -27,12 +36,12 @@ code_corrected = col2.code('''#Output Code''', language="python")
 if code:
     corrected_code_str, emissions_notcorrected, emissions_corrected, percent_reduction = pp.process(code_to_correct=code, country=country_code)
     code_corrected.code(corrected_code_str, language="python")
-    col2.text("Improvement: {}%".format(percent_reduction))
-
+    col2.text("Co2 emission saved: {}%".format(percent_reduction))
+    col2.text("Co2 emission left with code based in {}: {} kg".format(country_code, emissions_notcorrected))
 if code2:
     stringio = StringIO(code2.getvalue().decode("utf-8"))
     code_to_read = stringio.read()
-    col2.text("Improvement: {}%".format(code_to_read))
     corrected_code_str, emissions_notcorrected, emissions_corrected, percent_reduction = pp.process(code_to_correct=code_to_read, country=country_code)
     code_corrected.code(corrected_code_str, language="python")
-    col2.text("Improvement: {}%".format(percent_reduction))
+    col2.text("Co2 emission saved: {}%".format(percent_reduction))
+    col2.text("Co2 emission left with code based in {}: {} kg".format(country_code, emissions_notcorrected))
